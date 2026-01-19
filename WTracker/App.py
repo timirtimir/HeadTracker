ebddf7840c299f13ad3a4e024dfcccc98db3c9bb
timirtimir <tamerekim@yahoo.com>
2026-01-19 22:12:16 +0300

    Refactored and improved the HeadTracker class and created a new main App class

---
+++ WTracker/App.py
@@ -0,0 +1,7 @@
+from HeadTracker import HeadTracker
+
+def main():
+    HT = HeadTracker()
+    HT.run()
+if __name__ == "__main__":
+    main()
--- WTracker/HeadTracker.py
+++ WTracker/HeadTracker.py
@@ -1,79 +1,83 @@
 from basicFaceMeshModule import FaceMeshDetector
 import cv2 as cv
 import pyautogui
+import keyboard as kb
 import pyvjoy as vjoy
-
-def main():
-    # Initialize camera and vJoy constants
-    cam_w, cam_h = pyautogui.size()
-    x_scale, y_scale = 200, 120
-    prev_x, prev_y = 0, 0
-    vjoy_x, vjoy_y = int(32767/2), int(32767/2)
-    deadzone_x, deadzone_y = 0, 0
-    deadzone_base = 0.1
-    initial_len_line = None 
-    below_threshold = False
-    threshold = 88
-    # Initialize camera
-    cap = cv.VideoCapture(0)
-    cap.set(3, cam_w)
-    cap.set(4, cam_h)
-
-    # Initialize FaceMeshDetector and vJoy device
-    detector = FaceMeshDetector(minDetectCon=0.7, minTrackCon=0.7)
-    print(detector.minDetectCon, detector.minTrackCon)
-    j = vjoy.VJoyDevice(1)
-
-    while True:
-        success, img = cap.read()
-        img = cv.flip(img, 1)
-        img, rel_x, rel_y = detector.get_relative_position(img, target_id=4, draw=False)
-        lm_list = detector.get_lm_list(img)
-        if len(lm_list) != 0:
-            x1, y1 = lm_list[386][1], lm_list[386][2]
-            x2, y2 = lm_list[374][1], lm_list[374][2]
-
-            cv.line(img, (x1, y1), (x2, y2), (0, 255, 0), 1)
-            len_line = ((x2 - x1) + (y2 - y1))
-            if initial_len_line is None:
-                initial_len_line = len_line
-            else:
-                line_percentage = len_line / initial_len_line * 100
-                if below_threshold and line_percentage >= threshold:
-                    pyautogui.press('y')
-                elif not below_threshold and line_percentage < threshold:
-                    pyautogui.press('y')
-                below_threshold = line_percentage < threshold
-        if rel_x is not None and rel_y is not None:
-            # Set percentage values based on the relative position
-            percentage_x = (rel_x / x_scale) * 100
-            percentage_y = (rel_y / y_scale) * -100
-            percentage_x = max(min(percentage_x, 100), -100)
-            percentage_y = max(min(percentage_y, 100), -100)
-
-            # Set deadzone values that increase linearly for smoothing
-            deadzone_x = deadzone_base + percentage_x/100
-            deadzone_y = deadzone_base + percentage_y/100
-
-            # Implement the deadzone logic and move the vJoy axes
-            if abs(percentage_x - prev_x) > deadzone_x:   
-                vjoy_x = int((percentage_x + 100) / 200 * 32767)
-                prev_x = percentage_x
-            if abs(percentage_y - prev_y) > deadzone_y:   
-                vjoy_y = int((percentage_y + 100) / 200 * 32767)
-                prev_y = percentage_y  
-            j.set_axis(vjoy.HID_USAGE_X, vjoy_x)
-            j.set_axis(vjoy.HID_USAGE_Y, vjoy_y)
-            
-            
-            
-    #     cv.imshow("Image", img)
-    #     if cv.waitKey(1) & 0xFF == ord('q'):
-    #         break
-    # cap.release()
-    # cv.destroyAllWindows()
-
-if __name__ == "__main__":
-    main()
+import time
 
 
+class HeadTracker:
+    def __init__(self, x_scale = 200, y_scale = 120, deadzone_base = 0.1, threshold = 88):
+        # Initialise camera and vJoy constants 
+        self.cam_w, self.cam_h = pyautogui.size()
+        self.prev_x, self.prev_y = 0, 0
+        self.vjoy_x, self.vjoy_y = 32768 // 2, 32768 // 2
+        self.deadzone_x, self.deadzone_y = 0, 0
+        self.initial_len_line = None
+        self.below_threshold = False
+
+        self.x_scale, self.y_scale = x_scale, y_scale
+        self.threshold = threshold
+        self.deadzone_base = deadzone_base
+
+        # Initialise camera
+        self.cap = cv.VideoCapture(0)
+        self.cap.set(3, self.cam_w)
+        self.cap.set(4, self.cam_h)
+
+        # Initialise FaceMeshDetector and vJoy device
+        self.detector = FaceMeshDetector(minDetectCon=0.7, minTrackCon=0.7)
+        self.j = vjoy.VJoyDevice(1)
+
+    def run(self):
+        while True:
+            success, img = self.cap.read()
+            if not success:
+                continue
+            img = cv.flip(img, 1)
+            img, rel_x, rel_y = self.detector.get_relative_position(img, target_id=4, draw=False)
+            lm_list = self.detector.get_lm_list(img)
+            # Logic for blink detection
+            if len(lm_list) != 0:
+                x1, y1 = lm_list[386][1], lm_list[386][2]
+                x2, y2 = lm_list[374][1], lm_list[374][2]
+
+                cv.line(img, (x1, y1), (x2, y2), (0, 255, 0), 1)
+                len_line = ((x2 - x1) + (y2 - y1))
+                if self.initial_len_line is None:
+                    self.initial_len_line = len_line
+                else:
+                    line_percentage = len_line / self.initial_len_line * 100
+                    if self.below_threshold and line_percentage >= self.threshold:
+                        pyautogui.press('y')
+                    elif not self.below_threshold and line_percentage < self.threshold:
+                        pyautogui.press('y')
+                    self.below_threshold = line_percentage < self.threshold
+            if rel_x is not None and rel_y is not None:
+                # Set percentage values based on the relative position
+                percentage_x = (rel_x / self.x_scale) * 100
+                percentage_y = (rel_y / self.y_scale) * -100
+                percentage_x = max(min(percentage_x, 100), -100)
+                percentage_y = max(min(percentage_y, 100), -100)
+
+                # Set deadzone values that increase linearly for smoothing
+                self.deadzone_x = self.deadzone_base + percentage_x/100
+                self.deadzone_y = self.deadzone_base + percentage_y/100
+
+                # Implement the deadzone logic and move the vJoy axes
+                if abs(percentage_x - self.prev_x) > self.deadzone_x:   
+                    self.vjoy_x = int((percentage_x + 100) / 200 * 32768)
+                    self.prev_x = percentage_x
+                if abs(percentage_y - self.prev_y) > self.deadzone_y:   
+                    self.vjoy_y = int((percentage_y + 100) / 200 * 32768)
+                    self.prev_y = percentage_y  
+                self.j.set_axis(vjoy.HID_USAGE_X, self.vjoy_x)
+                self.j.set_axis(vjoy.HID_USAGE_Y, self.vjoy_y)
+                time.sleep(0.004)
+
+            if kb.is_pressed('esc'):
+                break
+        # Exit program and recenter the joystick
+        self.cap.release()
+        self.j.set_axis(vjoy.HID_USAGE_X, 16384)
+        self.j.set_axis(vjoy.HID_USAGE_Y, 16384)
